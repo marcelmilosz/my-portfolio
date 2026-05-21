4164c54f20327590896d68aa73005e3032dbb881
marcelmilosz <marcel.milosz99@gmail.com>
2026-05-21 17:21:14 +0200

    feat: updated packages

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-        domains: [], // jeśli używasz zewnętrznych obrazów
+        domains: [],
     },
     webpack(config: Configuration) {
         config.module?.rules?.push({
